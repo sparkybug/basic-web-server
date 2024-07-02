23488d64da990035d6744a65b4bb729a02ea4d59
sinach <ukaegbuosinachi15@gmail.com>
2024-07-02 09:10:06 +0100

    fixing issues

--- api/app.js
+++ api/app.js
@@ -7,6 +7,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Trust the X-Forwarded-For header
 app.set('trust proxy', true);
 
@@ -16,20 +19,25 @@ app.get('/api/', async(req, res) => {
 
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Guest';
-    // const ip = req.ip === '::1' ? '127.0.0.1' : req.ip;
-    let clientIp = req.ip;
+
+    if (!visitorName) {
+        throw new Error('Visitor name is required');
+    }
+
+    let clientIp = req.ip || req.connection.remoteAddress;
+    // let formattedIp = ip.toString(clientIp);
 
     console.log(`Client IP: ${clientIp}`);
 
     if (clientIp === '::1' || clientIp === '127.0.0.1') {
         clientIp = '8.8.8.8'; 
-    }else {
-        clientIp = ip.toString(clientIp);
+    } else {
+        let formattedIp = ip.toString(clientIp);
     } 
 
     try {
         // Get location data from IPinfo
-        const locationResponse = await axios.get(`https://ipinfo.io/${clientIp}?token=${process.env.IPINFO_TOKEN}`);
+        const locationResponse = await axios.get(`https://ipinfo.io/${formattedIp}?token=${process.env.IPINFO_TOKEN}`);
 
         console.log(locationResponse);
 
@@ -62,7 +70,7 @@ app.get('/api/hello', async (req, res) => {
 
         res.status(201).json(response);
     } catch (error) {
-        console.error(error);
+        console.error('Error handling /api/hello route', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
